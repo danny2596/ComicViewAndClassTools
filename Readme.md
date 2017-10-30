e11255bd8e624e19edc7043d1924e480bce54e1f
danny2596 <danny2596@gmail.com>
2017-10-30 22:40:40 +0800

    add readme, src/main.py

---
+++ Readme.md
@@ -0,0 +1,5 @@
+Python GUI (Tkinter) 練習
+目標
+python Tkinter 練習
+檔案管理練習
+
---
+++ src/main.py
@@ -0,0 +1,10 @@
+from tkinter import *
+def main_function():
+	print("----main function start----")
+	
+	
+	print("----main function done----")
+
+
+###### call main_function #####
+main_function()
